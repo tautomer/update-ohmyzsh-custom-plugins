6d513668288da1238b047e6aa8d0af6ad18e1b14
Xinyang Li <soybeanyoung@gmail.com>
2019-10-30 17:52:14 -0400

    check if the repo is up-to-date first

--- update-custom-plugins.plugin.zsh
+++ update-custom-plugins.plugin.zsh
@@ -1,19 +1,25 @@
 function update-custom-plugins() {
 
     omz_url=$(git -C $ZSH_CUSTOM config --get remote.origin.url)
+    # find all 2nd-level directories in $ZSH_CUSTOM
+    dirs=$(find $ZSH_CUSTOM -mindepth 2 -maxdepth 2 -type d)
 
-    function update() {
-		dirs=$(find $1 -maxdepth 1 -mindepth 1 -type d)
-        for i in $(echo $dirs)
-        do
-            url=$(git -C $i config --get remote.origin.url)
-            if [[ -n $url ]] && [[ $url != $omz_url ]]; then
-                echo "Checking updte for $(basename $i)"
+    for i in $(echo $dirs)
+    do
+        url=$(git -C $i config --get remote.origin.url)
+        # skip if the folder is not a git repo
+        if [[ -n $url ]] && [[ $url != $omz_url ]]; then
+            echo "Checking update for $(basename $i)"
+            git -C $i remote update > /dev/null
+            nc=$(git -C $i log HEAD..origin/master --oneline | wc -l)
+            if [[ $nc != 0 ]]; then
+                echo "The local version is $nc commits behind the remote"
                 git -C $i pull origin master
+            else
+            echo "$(basename $i) is up-to-date"
             fi
-        done
-    }
-
-    update $ZSH_CUSTOM/plugins
-    update $ZSH_CUSTOM/themes
+        else
+            echo "Skip $(basename $i) which is NOT a git repo"
+        fi
+    done
 }
